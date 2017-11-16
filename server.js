b52b122a63d79a8e9175aa85075c5e23760b4b2e
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2017-11-16 17:21:44 +0200

    Fixed server

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const path = require('path');
 const http = require('http');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 // Get our API routes
 const api = require('./server/routes/api');
 
-const app = require('express')();
+const app = express();
 
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'moviedb.webapp')));
+app.use(express.static(path.join(__dirname, 'moviedb.webapp/dist')));
 
 // Set our api routes
 app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'moviedb.webapp/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 /**
