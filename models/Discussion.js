99cbcc3236c417f7eb0be0862bd15c9d5c4ad4b6
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2018-02-10 12:00:35 +0200

    fixed discussion CRU

--- models/Discussion.js
+++ models/Discussion.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const discussionSchema = mongoose.Schema({
     subject: String,
     comments: []
-});
+}, {
+    usePushEach: true
+  });
 const Discussion = mongoose.model('Discussion', discussionSchema);
 
 module.exports = Discussion;
