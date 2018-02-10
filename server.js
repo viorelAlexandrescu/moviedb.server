2f0666afa762cfcb54f4070dc98471e4e147a40d
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2018-02-10 08:14:49 +0200

    Preped for deploy

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb://192.168.100.2:27017/ip')
+mongoose.connect('mongodb://localhost:27017/ip')
         .then(() => console.log('connected to db'))
         .catch((err) => console.log('could not connect to db', err));
 //Set Port
 const port = process.env.PORT || '5000';
 app.set('port', port);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
   
 
 // TODO add db connection call
