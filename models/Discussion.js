90474bc5221f5cd71ddbcd7237df344391ddbf9e
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2018-02-10 10:39:10 +0200

    fixed bug in model

--- models/Discussion.js
+++ models/Discussion.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const discussionSchema = mongoose.Schema({
-    subject: string,
+    subject: String,
     comments: []
 });
 const Discussion = mongoose.model('Discussion', discussionSchema);
